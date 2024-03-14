f93122a3c1110c0920eaeab792b1d743ee28eadf
Paritosh-Pranjal <paritosh.pranjal@zemosolabs.com>
2024-03-14 17:31:36 +0530

    add frontend and backend workflows

--- frontend/src/services/API.ts
+++ frontend/src/services/API.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
 export default axios.create({
-  baseURL: "http://localhost:8090",
+  baseURL: "http://44.192.42.127:8090",
 });
 console.log(process.env.REACT_APP_API_ENDPOINT);
